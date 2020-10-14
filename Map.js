a7c50da9e1248a1e2c9fa67051c71b7daa0b51b9
Nouf-2020 <70623133+Nouf-2020@users.noreply.github.com>
2020-10-14 16:35:08 +1100

    fix map error

--- Map.js
+++ Map.js
@@ -1,35 +1,57 @@
 import React from 'react'
-import { StyleSheet} from 'react-native';
-import MapView, { PROVIDER_GOOGLE,Marker} from 'react-native-maps';
-import { locations } from './resources/MapData';
+import { View, StyleSheet, Image, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker , Callout} from 'react-native-maps';
+
+
 class Map extends React.Component {
    
     render() {
         return (
-         
-                <MapView
-                    provider={PROVIDER_GOOGLE}
-                    style={styles.map}
-                    region={{
-                        latitude: -35.35000,
-                        longitude: 149.16670,
-                        latitudeDelta: 0.09,
-                        longitudeDelta: 0.035,
-                    }}
-                >
-                    {
-                    locations.map(marker => (
-                            <Marker
-                                coordinate={{latitude: marker.latitude,longitude: marker.longitud}}
-                                title={marker.title}
-                                description={marker.description}
-                                markerImage={marker.markerImage}
-                            />
-                            ))
-                    }
 
-           </MapView>
-          
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                region={{
+                    latitude: -35.35000,
+                    longitude: 149.16670,
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.035,
+                }}
+            >
+                <Marker
+                    coordinate={{ latitude: -35.267758, longitude: 149.079150, }}
+                    title="Aranda Bushlland Nuture Reserve"
+                    description="Aranda Bushland is an important part of the wooded area which extends from Black Mountain to the Molonglo and Murrumbidgee river corridors."
+                    image={require('../assets/park1.jpg')}
+
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: - 35.269278,
+                            longitude: 149.163692,
+                        }}
+                        title="Mount Ainslie Nature Reserve"
+                        description="Mount Ainslie Nature Reserve (637 hectares) and the adjoining Mount Majura Nature Reserve form a significant ridge in north-east Canberra."
+                        image={require('../assets/park2.jpg')}
+                    >
+                        <Callout tooltip>
+                            <View>
+                                <View style={styles.bubble}>
+                                    <Text style={styles.name}>Favourite Restaurant</Text>
+
+                                    <Image
+                                        style={styles.image}
+                                        source={require('../assets/park3.jpg')}
+                                    />
+                                </View>
+                                <View style={styles.arrowBorder} />
+                                <View style={styles.arrow} />
+                            </View>
+                        </Callout>
+                    </Marker>
+                </Marker>
+            </MapView>
+
         );
     }
 };
@@ -39,8 +61,45 @@ const styles = StyleSheet.create({
     map: {
         height: '100%'
     },
-   
-
+     // Callout bubble
+   bubble: {
+        flexDirection: 'column',
+        alignSelf: 'flex-start',
+        backgroundColor: '#fff',
+        borderRadius: 6,
+        borderColor: '#ccc',
+        borderWidth: 0.5,
+        padding: 15,
+        width: 150,
+    },
+    // Arrow below the bubble
+    arrow: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#fff',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -32,
+    },
+    arrowBorder: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#007a87',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -0.5,
+        // marginBottom: -15
+    },
+    // Character name
+    name: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    // Character image
+    image: {
+        width: "100%",
+        height: 80,
+    },
 });
 
 export default Map;
--- resources/MapData.js
+++ resources/MapData.js
@@ -4,7 +4,7 @@ export const locations = [
         longitude: 149.079150,
         title: "Aranda Bushlland Nuture Reserve",
         description: "Aranda Bushland is an important part of the wooded area which extends from Black Mountain to the Molonglo and Murrumbidgee river corridors.",
-        markerImage: "../assets/park1.jpg",
+        markerImage: "../assets/park1.jpg"
 
     },
     {
@@ -12,14 +12,14 @@ export const locations = [
         longitude: 149.163692,
         title: "Mount Ainslie Nature Reserve",
         description: "Mount Ainslie Nature Reserve (637 hectares) and the adjoining Mount Majura Nature Reserve form a significant ridge in north-east Canberra.",
-        markerImage: "../assets/park2.jpg",
+        markerImage: "../assets/park2.jpg"
     },
     {
         latitude: -35.267758,
         longitude: 149.119234,
         title: "Red Hill Nature Reserve",
-        description: "Red Hill Nature Reserve (293 hectares) is a prominent hill located between Woden Valley and south Canberra. It makes an impressive ‘Bush Capital’ back drop to Parliament House.",
-        markerImage: "../assets/park3.jpg",
+        description: "Red Hill Nature Reserve (293 hectares) is a prominent hill located between Woden Valley and south Canberra.It makes an impressive ‘Bush Capital’ back drop to Parliament House.",
+        markerImage: "../assets/park3.jpg"
     },
 
-];
+]
